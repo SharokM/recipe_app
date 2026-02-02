6fec8ccfd374157dd309bf183186ce1b2b2ca694
sharok_m <sharok.d.mcdonnaugh@gmail.com>
2026-02-02 17:48:18 +0000

    db updated, error fixes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./components/Header";
 import "./App.css";
 
 function App() {
+
+  const [recipes, setRecipes] = useState([]);
+
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("/api/recipes")
+      const data = await response.json()
+      setRecipes(data);
+    } catch (error) {
+      console.error("Error fetching recipes:", error);
+      console.log("Using fallback data.");
+    }
+  }
+
+  useEffect(()=> {
+    // code 
+  }, [fetchData]);
+
   return (
     <div className='recipe-app'>
       <Header />
