05730b6020562ac6e06752e3fd7c1471e75f3c6c
sharok_m <sharok.d.mcdonnaugh@gmail.com>
2026-02-03 19:33:09 +0000

    front end hooks & placeholders, useEff added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
 import React, {useState, useEffect} from "react";
 import Header from "./components/Header";
 import "./App.css";
+import RecipeExcerpt from "./components/RecipeExcerpt";
 
 function App() {
 
   const [recipes, setRecipes] = useState([]);
 
-
-  const fetchData = async () => {
-    try {
-      const response = await fetch("/api/recipes")
-      const data = await response.json()
-      setRecipes(data);
-    } catch (error) {
-      console.error("Error fetching recipes:", error);
-      console.log("Using fallback data.");
-    }
-  }
-
   useEffect(()=> {
-    // code 
-  }, [fetchData]);
+    const fetchAllRecipes = async () => {
+      try {
+        const response = await fetch("/api/recipes")
+        if (response.ok) {
+          const data = await response.json()
+          setRecipes(data);
+        } else {
+          console.log("Failed to fetch recipes, using fallback data."); 
+        }
+      } catch (error) {
+        console.error("Error fetching recipes:", error);
+        console.log("Using fallback data.");
+      }
+    }
+    fetchAllRecipes()
+  }, []);
 
   return (
     <div className='recipe-app'>
       <Header />
-      <p>Your recipes here! </p>
+      <div className="recipe-list">
+
+      
+      {recipes.map((recipe) => {
+        return < RecipeExcerpt key={recipe.id} recipe={recipe} />
+})}
+      </div>
     </div>
   );
 }
