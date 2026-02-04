437d7c28712b3dde7615a21a0b6dc85e4182e864
sharok_m <sharok.d.mcdonnaugh@gmail.com>
2026-02-04 21:23:05 +0000

    onClick handles for cancel and add recipe set up, ready to config

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import Header from "./components/Header";
 import "./App.css";
 import RecipeExcerpt from "./components/RecipeExcerpt";
 import ReceipeFull from "./components/RecipeFull";
+import NewRecipeForm from "./components/NewRecipeForm";
 
 function App() {
 
   const [recipes, setRecipes] = useState([]);
 
   const [selectedRecipe, setSelectedRecipe] = useState(null);
+
+  const [newRecipe, setNewRecipe] = useState({
+    
+    title: "",
+    ingredients: "",
+    instructions: "",
+    servings: 1, // conservative default
+    description: "",
+    image_url: "https://images.pexels.com/photos/9986228/pexels-photo-9986228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  
+})
+
+  const [showNewRecipeForm, setShowNewRecipeForm] = useState(false)
 
   useEffect(()=> {
     const fetchAllRecipes = async () => {
@@ -36,9 +50,20 @@ function App() {
     setSelectedRecipe(null)
   }
 
+
+  const hideRecipeForm = () => {
+    showNewRecipeForm(false)
+  }
+
+  const showRecipeForm = () => {
+    showNewRecipeForm(true)
+    selectedRecipe(null)
+  }
+
   return (
     <div className='recipe-app'>
       <Header />
+      {showNewRecipeForm && <NewRecipeForm newRecipe={newRecipe} hideRecipeForm={hideRecipeForm} />}
       {selectedRecipe && 
       <ReceipeFull 
       selectedRecipe={selectedRecipe} 
